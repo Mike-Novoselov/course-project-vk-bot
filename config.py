3e999c277cb4e5f4ace99d8015e7b9047f8b70ae
Mike-Novoselov <68411205+Mike-Novoselov@users.noreply.github.com>
2023-06-15 11:41:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-host = 'localhost'
-user = 'postgres'
-password = 'ghbdtnsql'
-db_name = 'student_diplom'
+host = '***'
+user = '***'
+password = '***'
+db_name = '***'
 
-'postgresql+psycopg2://postgres:ghbdtnsql@localhost:5432/student_diplom'
+user_token = '***'
 
-user_token = 'vk1.a.LDCGK4dnJHsMEFc05jS6_86yDuddbded6kFywWUjOePqayR7FRwgGUjV-Cn95040iHXXfj-SCwdvb5vgZjgTmUX58G5a48u2fgbyjtdErNYHzZbKwL_dQXWUpxetyxFTH64ZhNC3ThHlDN8lyRYzWIlX2nlnGek_Sg6zuZci4P6oGtjCD_7EoVbeydSGSTyBQ2uR2IyQl1nD8UmqphVb3g'
-
-group_token = 'vk1.a.2apoLbfp_2hCMymaw5-9Gt90Z5dBgtks6SCbn9VUhxbFTqq4p1xHXbPC9KtT9AXHQQ4Ie8BHn8-LsD_O2aHz82mf4xmBuoaQW2Mvsw4Rki_IBsOMSqceXMHEUjbG8qYGbmRE0xIaX-ejTPZRUu0Gaju0F-rbC7sUN57tE-dhp9G2lZ9zkQE38ixv7h_o0I3hgdh3Wno-83gGEWoUngUcpA'
+group_token = '***'
